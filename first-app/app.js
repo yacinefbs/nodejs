61a2291b37a04346ab157cd703b578e97276605c
yacinefbs <naouiyassine009@gmail.com>
2018-11-11 20:05:09 +0000

    Fin : OS Module

--- first-app/app.js
+++ first-app/app.js
@@ -1,6 +1,21 @@
-const path = require('path');
+const os = require('os');
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+// console.log('Total Memory : ' + totalMemory);
+
+
+//Template string
+
+//ES6 / ES2015 : ECMAScript 6
+
+console.log(`Total Memory : ${totalMemory}`);
+console.log(`Free Memory : ${freeMemory}`);
+
+
+/* const path = require('path');
 var pathObj = path.parse(__filename);
 console.log(pathObj);
-
+ */
 
 
