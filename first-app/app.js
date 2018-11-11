1f4daaefb0aae40c7107ab80e88270e3fdf09e49
yacinefbs <naouiyassine009@gmail.com>
2018-11-11 20:22:33 +0000

    Fin : Event Arguments

--- first-app/app.js
+++ first-app/app.js
@@ -1,16 +1,15 @@
-const EventEmitter = require('events');
+const EventEmitter = require('events'); 
 
 const emitter = new EventEmitter();
 
 //Register a listener
-emitter.on('messageLogged', function(){
-    console.log('Listener called');
+emitter.on('messageLogged', function(arg){ 
+    console.log('Listener called', arg);
 });
 
 
 //Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {id : 1, url : 'http://'});
 
 
-
-
+//Raise: logging (data: message)
