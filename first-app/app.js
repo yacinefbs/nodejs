f56f43a1784a0c6269eb754490cf8eab34ceab14
yacinefbs <naouiyassine009@gmail.com>
2018-11-11 20:12:02 +0000

    Fin : File System Module

--- first-app/app.js
+++ first-app/app.js
@@ -1,21 +1,9 @@
-const os = require('os');
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+const fs = require('fs');
 
-// console.log('Total Memory : ' + totalMemory);
+const files = fs.readdirSync('./');
+console.log(files);
 
-
-//Template string
-
-//ES6 / ES2015 : ECMAScript 6
-
-console.log(`Total Memory : ${totalMemory}`);
-console.log(`Free Memory : ${freeMemory}`);
-
-
-/* const path = require('path');
-var pathObj = path.parse(__filename);
-console.log(pathObj);
- */
-
-
+fs.readdir('$', function(err, files){
+    if(err) console.log('Error', err);
+    else console.log('Result', files);
+});
